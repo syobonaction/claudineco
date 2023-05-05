1597922485133a6f4e67e3da6b8d373a9eeb361b
Charles Cole <sismondi@Nicolette.local>
2023-05-05 19:55:56 +0200

    Removed stray comma

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <>
         <ReCaptchaProvider reCaptchaKey={siteKey}>
             <Component {...pageProps} />
-        </ReCaptchaProvider>,
+        </ReCaptchaProvider>
       </>
     )
 }
